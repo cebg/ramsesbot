db36c135f1657e27163ac148ba2bcd60a7f7461a
Fernandez <matthieu2mdmail@gmail.com>
2020-11-28 21:49:32 +0100

    update fix

--- index.js
+++ index.js
@@ -333,11 +333,11 @@ Client.on("message", msg => {
 
 
         // -----------------------------------Fin MUTE+TEMPMUTE+KICK via commandes----------------------------
-         else if (!member.hasPermission("ADMINISTRATOR")) {
+        /* else if (!member.hasPermission("ADMINISTRATOR")) {
              msg.reply("Tu n'as pas la permission d'effectuer cette commande.")
          }
         // si un jour je trouve le moyen de faire marcher cette commande je bondis de joie (en gros si tu tentes de faire la commande mais que t'es pas admin ca t'envoie un message d'erreur.)
-    }
+    }*/
     //------------------------------------Fin KICK via commandes------------------------------------------
 
 
